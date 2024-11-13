3e78bff0e16c260c81e36dfb2525b9a940bfa7ff
tomasarrachea <tomas.arrachea@lambdaclass.com>
2024-11-13 12:58:49 -0300

    fmt

--- services/bls_aggregation/blsagg.go
+++ services/bls_aggregation/blsagg.go
@@ -390,7 +390,8 @@ func (a *BlsAggregatorService) singleTaskAggregatorGoroutineFunc(
 				signersApkG2 := bls.NewZeroG2Point()
 				signersAggSigG1 := bls.NewZeroSignature()
 				// for each quorum the operator has stake in, the signature is aggregated
-				// see https://github.com/Layr-Labs/eigenlayer-middleware/blob/7d49b5181b09198ed275783453aa082bb3766990/src/BLSSignatureChecker.sol#L161-L168
+				// see
+				// https://github.com/Layr-Labs/eigenlayer-middleware/blob/7d49b5181b09198ed275783453aa082bb3766990/src/BLSSignatureChecker.sol#L161-L168
 				for range operatorsAvsStateDict[signedTaskResponseDigest.OperatorId].StakePerQuorum {
 					signersApkG2 = signersApkG2.Add(
 						operatorsAvsStateDict[signedTaskResponseDigest.OperatorId].OperatorInfo.Pubkeys.G2Pubkey,
@@ -413,7 +414,6 @@ func (a *BlsAggregatorService) singleTaskAggregatorGoroutineFunc(
 					"taskResponseDigest", taskResponseDigest)
 
 				digestAggregatedOperators.signersOperatorIdsSet[signedTaskResponseDigest.OperatorId] = true
-
 				// for each quorum the operator has stake in, the signature is aggregated
 				for quorumNum, stake := range operatorsAvsStateDict[signedTaskResponseDigest.OperatorId].StakePerQuorum {
 					digestAggregatedOperators.signersAggSigG1.Add(signedTaskResponseDigest.BlsSignature)
