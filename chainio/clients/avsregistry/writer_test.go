3a10dc2126c15cc3ae18fad1a8e71f987f93fe0b
Pablo Deymonnaz <deymonnaz@gmail.com>
2024-12-06 17:17:59 -0300

    Compliance test for BLS signature

--- chainio/clients/avsregistry/writer_test.go
+++ chainio/clients/avsregistry/writer_test.go
@@ -6,10 +6,12 @@ import (
 
 	chainioutils "github.com/Layr-Labs/eigensdk-go/chainio/utils"
 	"github.com/Layr-Labs/eigensdk-go/crypto/bls"
+	"github.com/Layr-Labs/eigensdk-go/testutils"
 	"github.com/Layr-Labs/eigensdk-go/testutils/testclients"
 	"github.com/Layr-Labs/eigensdk-go/types"
 	gethcommon "github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -71,4 +73,33 @@ func TestWriterMethods(t *testing.T) {
 		require.NoError(t, err)
 		require.NotNil(t, receipt)
 	})
+}
+
+func TestAvsRegistryWriterTest_ComplianceBLSSignature(t *testing.T) {
+	// read input from JSON if available, otherwise use default values
+	var defaultInput = struct {
+		messageBytes []byte `json:"message_bytes"`
+		bls_priv_key string `json:"bls_priv_key"`
+	}{
+		messageBytes: []byte("Hello, world!Hello, world!123456"),
+		bls_priv_key: "12248929636257230549931416853095037629726205319386239410403476017439825112537",
+	}
+
+	testData := testutils.NewTestData(defaultInput)
+
+	// The message to sign
+	var messageArray [32]byte
+	copy(messageArray[:], testData.Input.messageBytes[:32])
+
+	// The private key as a string
+	privKey, _ := bls.NewPrivateKey(testData.Input.bls_priv_key)
+	keyPair := bls.NewKeyPair(privKey)
+
+	sig := keyPair.SignMessage(messageArray)
+
+	x := sig.G1Affine.X.String()
+	y := sig.G1Affine.Y.String()
+
+	assert.Equal(t, x, "15790168376429033610067099039091292283117017641532256477437243974517959682102")
+	assert.Equal(t, y, "4960450323239587206117776989095741074887370703941588742100855592356200866613")
 }
